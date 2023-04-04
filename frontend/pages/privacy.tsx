e45265a130116c56666a0aa504b4055a189d95db
Dakota Schramm <william.schramm11@gmail.com>
2023-04-04 13:55:12 -0500

    Added basic html structure for privacy and tos.

---
+++ frontend/pages/privacy.tsx
@@ -0,0 +1,97 @@
+import Head from 'next/head';
+
+const Privacy = () => {
+  return (
+    <>
+      <Head>
+        <title>Privacy Policy | Your Website Name</title>
+      </Head>
+      <div>
+        <h1>Privacy Policy</h1>
+
+        <p>
+          Your privacy is important to us. It is [Your Website Name]'s policy to
+          respect your privacy regarding any information we may collect from you
+          across our website, [Your Website URL], and other sites we own and
+          operate.
+        </p>
+
+        <h2>Information We Collect</h2>
+        <p>
+          We only collect information about you if we have a reason to do so–for
+          example, to provide our services, to communicate with you, or to make
+          our services better.
+        </p>
+        <p>
+          We collect this information from three sources: if and when you
+          provide information to us, automatically through operating our
+          services, and from outside sources.{' '}
+        </p>
+        <p>
+          The information we collect depends on the services you use, and may
+          include your name, email address, phone number, and IP address.
+        </p>
+
+        <h2>How We Use Information</h2>
+        <p>We use the information we collect in various ways, including to:</p>
+        <ul>
+          <li>Provide, operate, and maintain our website</li>
+          <li>Improve, personalize, and expand our website</li>
+          <li>Understand and analyze how you use our website</li>
+          <li>Develop new products, services, features, and functionality</li>
+          <li>
+            Communicate with you, either directly or through one of our
+            partners, including for customer service, to provide you with
+            updates and other information relating to the website, and for
+            marketing and promotional purposes
+          </li>
+          <li>Send you emails</li>
+          <li>Find and prevent fraud</li>
+        </ul>
+
+        <h2>Third-Party Services</h2>
+        <p>
+          We may use third-party services to help us operate our website, such
+          as hosting services, email delivery services, and analytics services.
+          These third-party service providers may have access to your personal
+          information in order to perform their functions, but they are
+          obligated to maintain the confidentiality and security of that
+          information and are restricted from using it for any other purpose.
+        </p>
+
+        <h2>Cookies</h2>
+        <p>
+          A cookie is a small piece of data that is stored on your computer or
+          mobile device. We use cookies to personalize content and ads, to
+          provide social media features, and to analyze our traffic. We also
+          share information about your use of our site with our social media,
+          advertising, and analytics partners who may combine it with other
+          information that you've provided to them or that they've collected
+          from your use of their services. You consent to our cookies if you
+          continue to use our website.
+        </p>
+
+        <h2>Security</h2>
+        <p>
+          We take the security of your personal information seriously and have
+          implemented measures to protect it from unauthorized access, use,
+          alteration, or disclosure.
+        </p>
+
+        <h2>Changes to This Privacy Policy</h2>
+        <p>
+          We may update our Privacy Policy from time to time. We will notify you
+          of any changes by posting the new Privacy Policy on this page.
+        </p>
+
+        <h2>Contact Us</h2>
+        <p>
+          If you have any questions or concerns about our Privacy Policy, please
+          contact us at [Your Contact Email].
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Privacy;
---
+++ frontend/pages/tos.tsx
@@ -0,0 +1,58 @@
+import Head from 'next/head';
+
+const Terms = () => {
+  return (
+    <>
+      <Head>
+        <title>Terms of Service | Your Website Name</title>
+      </Head>
+      <div>
+        <h1>[Your Website Name] Terms of Service</h1>
+          <p>By accessing the website at [website URL], you are agreeing to be bound by these terms of service, all applicable laws, and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site.</p>
+
+          <h2>Use License</h2>
+          <p>Permission is granted to temporarily download one copy of the materials (information or software) on [Your Website Name]'s website for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license, you may not:</p>
+          <ul>
+            <li>modify or copy the materials;</li>
+            <li>use the materials for any commercial purpose or for any public display (commercial or non-commercial);</li>
+            <li>attempt to decompile or reverse engineer any software contained on [Your Website Name]'s website;</li>
+            <li>remove any copyright or other proprietary notations from the materials; or</li>
+            <li>transfer the materials to another person or "mirror" the materials on any other server.</li>
+          </ul>
+          <p>This license shall automatically terminate if you violate any of these restrictions and may be terminated by [Your Website Name] at any time. Upon terminating your viewing of these materials or upon the termination of this license, you must destroy any downloaded materials in your possession whether in electronic or printed format.</p>
+
+          <h2>Disclaimer</h2>
+          <p>The materials on [Your Website Name]'s website are provided on an 'as is' basis. [Your Website Name] makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties including, without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights.</p>
+          <p>Further, [Your Website Name] does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its website or otherwise relating to such materials or on any sites linked to this site.</p>
+
+          <h2>Limitations</h2>
+          <p>In no event shall [Your Website Name] or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption) arising out of the use or inability to use the materials on [Your Website Name]'s website, even if [Your Website Name] or a [Your Website Name] authorized representative has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.</p>
+
+          <h2>Accuracy of Materials</h2>
+          <p>The materials appearing on [Your Website Name]'s website could include technical, typographical, or photographic errors. [Your Website Name] does not warrant that any of the materials on its website are accurate, complete or current. [Your Website Name] may make changes to the materials contained on its website at any time without notice. However, [Your Website Name] does not make any commitment to update the materials.</p>
+
+          <h2>Links</h2>
+          <p>[Your Website Name] has not reviewed all of the sites linked to its website and is not responsible for the contents of any such linked site. The inclusion of any link does not imply endorsement by [Your Website Name] of the site. Use of any such linked website is at the user's own risk.</p>
+
+          <h2>Modifications</h2>
+          <p>[Your Website Name] may revise these terms of service for its website at any time without notice. By using this website, you are agreeing to be bound by the then current version of these terms of service.</p>
+
+          <h2>Governing Law</h2>
+          <p>These terms and conditions are governed by and construed in accordance with the laws of [Your Country or State] and you irrevocably submit to the exclusive jurisdiction of the courts in that State or location.</p>
+
+          <h2>Privacy Policy</h2>
+          <p>Please review our <a href="[link to privacy policy]">Privacy Policy</a>, which also governs your use of [Your Website Name], to understand our practices.</p>
+
+          <h2>Termination</h2>
+          <p>[Your Website Name] reserves the right, in its sole discretion, to terminate your access to the website and the related services or any portion thereof at any time, without notice.</p>
+
+          <h2>Entire Agreement</h2>
+          <p>These terms and conditions constitute the entire agreement between you and [Your Website Name] in relation to your use of this website and supersede all prior agreements and understandings, whether written or oral, in relation to the website.</p>
+          <p>By using this website, you are agreeing to these terms and conditions. If you do not agree with these terms and conditions, you should not use this website.</p>
+
+      </div>
+    </>
+  );
+};
+
+export default Terms;
